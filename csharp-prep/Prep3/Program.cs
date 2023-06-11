8a3f6060f6c00946fe0f6ca2ec116037465240db
jpaulocts <jpaulocts@gmail.com>
2023-06-11 02:19:43 -0300

    Finish Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        
+        int guess ;
+        int number;
+        Random randomGenerator = new Random();
+        number = randomGenerator.Next(0, 100);
+        int Counter = 0;
+        bool play = true;
+        do
+
+        {
+            
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            Counter += 1;
+            if ( guess > number) {
+
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < number) {
+                Console.WriteLine("Higher");
+            }
+
+            else {
+
+                Console.WriteLine($"You made {Counter} tries.");
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine("Do you want play again? (y or n)");
+                string again = Console.ReadLine();
+
+                if (again == "n") {
+
+                    play = false;
+                    Console.WriteLine("Game Over!");
+
+                }
+
+                else {
+
+                    Console.WriteLine("Let's play again!");
+                    play = true;
+                    number = randomGenerator.Next(0, 100);
+                    Counter = 0;
+                    
+                }
+
+            }
+        }  while (guess != number || play == true);
     }
 }
