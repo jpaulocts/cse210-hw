42cc39a97bd793d9a9b6f5b45ae830a835ca7042
jpaulocts <jpaulocts@gmail.com>
2023-06-25 03:37:17 -0300

    Prove 3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference ref1 = new Reference("John",3,16);
+        Scripture scrip1 = new Scripture(ref1, "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+        string line = "enter";
+        Reference ref2 = new Reference("Proverbs", 3,5,6);
+        Scripture scrip2 = new Scripture(ref2,"Trust in the LORD with all your heart and lean not on your own understanding;in all your ways submit to him, and he will make your paths straight");
+        Reference ref3 = new Reference("Phillipians", 4,13);
+        Scripture scrip3 = new Scripture(ref3, "I can do all this through him who gives me strength.");
+
+        List<Scripture> scripList = new List<Scripture>();
+
+        scripList.Add(scrip1);
+        scripList.Add(scrip2);
+        scripList.Add(scrip3);
+
+        Random sort = new Random();
+        int num = sort.Next(0,scripList.Count());
+
+        Scripture scripchosen = new Scripture(scripList[num].GetReference(), scripList[num].GetReference().GetDisplayText());
+        scripchosen = scripList[num];
+
+
+        bool hidden = scripchosen.IsCompletelyHidden();
+      
+        while ((line != "quit"))
+        {
+            string reference = scripchosen.GetReference().GetDisplayText();
+            string text = scripchosen.GetDisplayText();
+
+            Console.WriteLine($"{reference} - {text}");
+        
+
+            Console.WriteLine("Please enter to continue or quit to exit: ");
+            line = Console.ReadLine();
+
+            if ((line == "quit") || (hidden == true))
+            {
+                
+                Console.WriteLine("Game Over!");
+                line = "quit";
+
+            } else 
+            {
+                scripchosen.HideRandomWords();
+                hidden = scripchosen.IsCompletelyHidden();
+            }
+
+            Console.Clear();
+
+            
+
+            
+
+        }
+        
+
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture 
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+
+    public Scripture(Reference Reference, string text) 
+    {
+        _reference = Reference;
+
+        
+        string[] splitText = text.Split(' ');
+
+        foreach (string word in splitText)
+        {
+            Word phrase = new Word(word);
+
+            _words.Add(phrase);
+        }
+    }
+
+    public void HideRandomWords() 
+    {
+        Random rnd = new Random();
+        int number = rnd.Next(0, _words.Count());
+
+        
+        _words[number].Hide();
+
+        
+
+
+    
+        
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> str = new List<string>();
+
+        foreach (Word wordn in _words) 
+        
+        {
+            string a = wordn.GetDisplayText();
+            str.Add(a); 
+        }
+
+        string fullText = String.Join(' ', str);
+
+        return fullText;
+
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        bool fullHidden = true;
+
+         foreach (Word word in _words)
+            {
+                fullHidden = word.IsHidden();
+
+                if (fullHidden == false) 
+                {
+                    break;
+                }
+            }
+
+        return fullHidden;   
+    }
+
+    public Reference GetReference()
+    {
+        return _reference;
+    }
+    
+}
