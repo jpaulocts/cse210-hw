6a47f6c749a9ad7acbe002214b76748d1a3798b7
jpaulocts <jpaulocts@gmail.com>
2023-07-08 22:54:40 -0300

    Develop 5

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,76 @@
+using System;
+
+public abstract class Goal 
+{
+    private string _shortName;
+    private string _description;
+    private string _points;
+
+    private bool _Ishealth;
+    
+
+    public Goal(string name, string description, string points, bool health=true)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+
+    }
+
+    public abstract void RecordEvent();
+
+    public abstract bool IsComplete();
+
+    public virtual string GetDetailsString()
+    {
+       
+        
+        if (IsComplete())
+        {
+            return($"[x] {_shortName} ({_description}).");
+        
+        } else 
+        {
+            return($"[ ] {_shortName} ({_description}).");
+        }
+    }
+
+    public abstract string GetStringRepresentation();
+
+    public string GetName()
+    {
+        return _shortName;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public int GetPoints()
+    {
+        int point = int.Parse(_points);
+
+        return point;
+    }
+
+   public void SetPoints(int points)
+   {
+
+    _points = points.ToString();
+
+   } 
+   
+public bool SetHealth(string health)
+{
+    if (health == "no")
+    {
+        return _Ishealth = false;
+    } else {
+        
+        return _Ishealth= true;
+    }
+   
+}
+
+}
